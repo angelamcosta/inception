bb9caef505817a7ab9b57cea586fc077ad009c2d
Angela Lima <angelamcostalima@icloud.com>
2024-01-17 15:00:47 +0000

    🔨 update `/mariadb` script
    - fix lock error where Aria could not be installed/started properly

--- mariadb/tools/init-db.sh
+++ mariadb/tools/init-db.sh
@@ -2,8 +2,6 @@
 
 service mariadb start
 
-sleep 5
-
 echo "CREATE DATABASE IF NOT EXISTS $DB_NAME ;" > db1.sql
 echo "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD' ;" >> db1.sql
 echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' ;" >> db1.sql
@@ -12,4 +10,7 @@ echo "FLUSH PRIVILEGES ;" >> db1.sql
 
 mysql -u root --password=$ROOT_PASSWORD < db1.sql
 
-mysqld --user=mysql --console --skip-aria-engine
+# process needs to be killed (see the glossary in readme)
+kill $(cat /var/run/mysqld/mysqld.pid)
+
+mysqld --user=mysql --console
