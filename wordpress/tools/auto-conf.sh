7e844dfce01242878869e25632e012b1485e8584
Angela Lima <angelamcostalima@icloud.com>
2024-01-20 19:24:59 +0000

    🔨 update `/wordpress` script

--- wordpress/tools/auto-conf.sh
+++ wordpress/tools/auto-conf.sh
@@ -1,15 +1,35 @@
 #!/bin/bash
 
-# wait for MariaDB to start
-sleep 10
+mkdir -p /var/www/html
 
-if [ ! -f "/var/www/wordpress/wp-config.php" ]; then
-    wp config create --allow-root \
-        --dbname=$DB_NAME \
-        --dbuser=$DB_USER \
-        --dbpass=$DB_PASSWORD \
-        --dbhost=mariadb:3306 --path='/var/www/wordpress'
+cd /var/www/html
 
-fi
+rm -rf *
+
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+mv wp-cli.phar /usr/local/bin/wp
+
+wp core download --allow-root
+mv wp-config-sample.php wp-config.php
+
+sed -i -e "s/database/$db_name/1" \
+       -e "s/database_user/$db_user/1" \
+       -e "s/passwod/$db_pwd/1" \
+       -e "s/localhost/mariadb/1" wp-config.php
+
+wp core install --url=$DOMAIN_NAME/ --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
+
+wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
+
+wp theme install altr --activate --allow-root
+
+wp plugin install redis-cache --activate --allow-root
+
+sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
+
+mkdir -p /run/php
+
+wp redis enable --allow-root
 
 /usr/sbin/php-fpm7.3 -F
