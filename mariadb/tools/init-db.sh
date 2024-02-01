0b8f6929927ab658df9b6c55328ae7bbef9fc8df
Angela Lima <angelamcostalima@icloud.com>
2024-02-01 15:01:40 +0000

    🚧 wip -> update `/mariadb` files

--- mariadb/tools/init-db.sh
+++ mariadb/tools/init-db.sh
@@ -2,11 +2,17 @@
 
 service mysql start
 
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+echo "Creating database: ${DB_NAME}"
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
+echo "Creating user: ${DB_USER}"
+mysql -e "CREATE USER IF NOT EXISTS \`${DB_USER}\`@'localhost' IDENTIFIED BY '${DB_PASSWORD}';"
+echo "Granting privileges on ${DB_NAME} to ${DB_USER}"
+mysql -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO \`${DB_USER}\`@'%' IDENTIFIED BY '${DB_PASSWORD}';"
+echo "Altering root user password"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_USER}';"
+echo "Flushing privileges"
 mysql -e "FLUSH PRIVILEGES;"
-mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
 
-exec mysqld_safe
+mysqladmin -u root -p$DB_PASSWORD shutdown
+
+exec mysqld
