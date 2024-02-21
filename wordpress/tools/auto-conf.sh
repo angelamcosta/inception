70e57d155bda1a9f747e2f83cbbe731030748ad1
Angela Lima <angelamcostalima@icloud.com>
2024-02-21 13:24:26 +0000

    ✨ update `/wordpress` folder

--- wordpress/tools/auto-conf.sh
+++ wordpress/tools/auto-conf.sh
@@ -11,16 +11,25 @@ if [ ! -e /var/www/html/wp-config.php ]; then
                     --allow-root
 
     wp core install --title=$WORDPRESS_TITLE \
-                    --admin_user=$WORDPRESS_ADMIN_USER \
+                    --admin_user=$DB_USER \
                     --admin_password=$DB_PASSWORD \
                     --admin_email=$WORDPRESS_ADMIN_EMAIL \
                     --url=$WORDPRESS_URL \
                     --allow-root
 
-    wp user create $WORDPRESS_ADMIN_USER $WORDPRESS_ADMIN_EMAIL \
+    wp user create $DB_USER $WORDPRESS_ADMIN_EMAIL \
                     --role=author --user_pass=$DB_PASSWORD \
                     --path='/var/www/html' >> /log.txt \
                     --allow-root
+    
+    wp user create $WORDPRESS_ADMIN $WORDPRESS_ADMIN_EMAIL \
+                    --role=administrator --user_pass=$DB_PASSWORD \
+                    --path='/var/www/html' >> /log.txt \
+                    --allow-root
+
+    wp plugin install https://downloads.wordpress.org/plugin/gutenberg.17.7.0.zip --activate --allow-root
+
+    wp theme activate twentytwentythree --allow-root
 fi
 
 if [ ! -d /run/php ]; then
